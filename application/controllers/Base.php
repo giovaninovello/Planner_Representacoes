29254dddda7d3068db937830be8e380d4c6149ae
giovani <ginfors@gmail.com>
2016-11-28 15:13:34 -0200

    PRIMEIRO COMMIT

---
+++ application/controllers/Base.php
@@ -0,0 +1,98 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Base extends CI_Controller {
+
+    function __construct()     {
+        parent::__construct();
+        /*
+         * Carrega a library 'session' para que seja possível fazer o uso
+         * das sessões na aplicação
+         */
+        $this->load->library('session');
+    }
+
+    /*
+     * Responsável por carregar a view com o formulário
+     */
+    public function Index()
+    {
+        $this->load->view('pag_inicial');
+    }
+
+    /*
+     * Responsável por enviar o email
+     */
+    public function EnviarEmail()
+    {
+        // Carrega a library email
+        $this->load->library('email');
+        //Recupera os dados do formulário
+        $dados = $this->input->post();
+
+        //Inicia o processo de configuração para o envio do email
+        $config['protocol'] = 'smtp'; // define o protocolo utilizado
+        $config['wordwrap'] = TRUE; // define se haverá quebra de palavra no texto
+        $config['validate'] = TRUE; // define se haverá validação dos endereços de email
+        $config['smtp_host']= 'ssl://smtp.googlemail.com';
+        $config['smtp_user']= 'ginfors@gmail.com';
+        $config['smtp_pass']= 'novello123456';
+        $config['smtp_port']= '465';
+        $config['charset']      = "iso-8859-1";
+        $config['mailtype']     = "html";
+        $config['newline']      = "\r\n";
+
+
+
+        /*
+         * Se o usuário escolheu o envio com template, define o 'mailtype' para html,
+         * caso contrário usa text
+         */
+        if(isset($dados['template']))
+            $config['mailtype'] = 'html';
+        else
+            $config['mailtype'] = 'text';
+
+        // Inicializa a library Email, passando os parâmetros de configuração
+        $this->email->initialize($config);
+
+        // Define remetente e destinatário
+        $this->email->from($dados['email'], $dados['nome']);
+        $this->email->to('giovani_tialvarosul@yahoo.com.br');
+
+
+        // Define o assunto do email
+        $this->email->subject($dados['assunto']);
+
+        /*
+         * Se o usuário escolheu o envio com template, passa o conteúdo do template para a mensagem
+         * caso contrário passa somente o conteúdo do campo 'mensagem'
+         */
+        if(isset($dados['template']))
+            $this->email->message($this->load->view('email-template',$dados, TRUE));
+        else
+            $this->email->message($dados['mensagem']);
+
+        /*
+         * Se foi selecionado o envio de um anexo, insere o arquivo no email
+         * através do método 'attach' da library 'Email'
+         */
+        if(isset($dados['anexo']))
+            $this->email->attach('./assets/images/unici/logo.png');
+
+        /*
+         * Se o envio foi feito com sucesso, define a mensagem de sucesso
+         * caso contrário define a mensagem de erro, e carrega a view home
+         */
+        if($this->email->send())
+        {
+            $this->session->set_flashdata('success','Email enviado com sucesso!');
+            $this->load->view('pag_inicial');
+        }
+        else
+        {
+            $this->session->set_flashdata('error',$this->email->print_debugger());
+            $this->load->view('pag_inicial');
+        }
+    }
+}
---
+++ application/views/pag_inicial.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, maximum-scale=1">
+	<title>Planner Representações Comerciais</title>
+	<link rel="icon" href="<?php echo base_url('assets/favicon.png')?>" type="image/png">
+	<link href="<?php echo base_url('assets/css/bootstrap.min.css"')?> rel="stylesheet" type="text/css">
+	<link href="<?php echo base_url('assets/css/style.css" rel="stylesheet"')?> type="text/css">
+	<link href="<?php echo base_url('assets/css/font-awesome.css" rel="stylesheet"')?> type="text/css">
+	<link href="<?php echo base_url('assets/css/animate.css" rel="stylesheet"')?> type="text/css">
+
+	<!--[if lt IE 9]>
+	<script src="/assets/js/respond-1.1.0.min.js"></script>
+	<script src="/assets/js/html5shiv.js"></script>
+	<script src="/assets/js/html5element.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+<!--Header_section-->
+<header id="header_wrapper">
+	<div class="container">
+		<div class="header_box">
+			<div class="logo"><a href="#"><img src="<?php echo base_url('assets/img/logo.png')?>" alt="logo"></a></div>
+			<nav class="navbar navbar-inverse" role="navigation">
+				<div class="navbar-header">
+					<button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				</div>
+				<div id="main-nav" class="collapse navbar-collapse navStyle">
+					<ul class="nav navbar-nav" id="mainNav">
+						<li class="active"><a href="#hero_section" class="scroll-link">Home</a></li>
+						<li><a href="#aboutUs" class="scroll-link">A Empresa</a></li>
+						<li><a href="#service" class="scroll-link">Serviços</a></li>
+						<li><a href="#clients" class="scroll-link">Parcerias</a></li>
+						<li><a href="#contact" class="scroll-link">Contato</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+</header>
+<!--Header_section-->
+
+<!--Hero_Section-->
+<section id="hero_section" class="top_cont_outer">
+	<div class="hero_wrapper">
+		<div class="container">
+			<div class="hero_section">
+				<div class="row">
+					<div class="col-lg-5 col-sm-7">
+						<div class="top_left_cont zoomIn wow animated">
+							<h2> <strong>Planner Representações Comerciais</strong></h2>
+							<p></p>
+							<a href="#service" class="read_more2">Leia Mais</a> </div>
+					</div>
+					<div class="col-lg-7 col-sm-5">
+						<img src="<?php echo base_url('assets/img/main_device_image.png"')?> class="zoomIn wow animated" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--Hero_Section-->
+
+<section id="aboutUs"><!--Aboutus-->
+	<div class="inner_wrapper">
+		<div class="container">
+			<h2>Empresa</h2>
+			<div class="inner_section">
+				<div class="row">
+					<div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right"><img src="<?php echo base_url('assets/img/about-img.jpg"')?> class="img-circle delay-03s animated wow zoomIn" alt=""></div>
+					<div class=" col-lg-7 col-md-7 col-sm-7 col-xs-12 pull-left">
+						<div class=" delay-01s animated fadeInDown wow animated">
+							<h3>Planner Representações Comerciais</h3><br/>
+							<p>Fundada em 2006 a Planner Representações Comerciais, iniciou suas atividades atendendo ao mercado de Analises clinicas, intermediando contratos de prestações de serviços para a realização de exames Clínicos  de alta complexidade,  tendo como representada grande empresa de Analises Clinicas por 11anos.</p> <br>
+							<p>Seu Sócio proprietário  atuou no mercado farmacêutico e cosméticos por 12 anos,  adquirindo larga experiência na divulgação e venda nos canais Pharma, hospitalares e distribuidores, ocupando cargo executivo de vendas em empresa Farmacêutica Multinacional por 12 anos, o que  credencia para desenvolver a atividade de representação comercial nestes Canais.</p>
+
+							.</p>Hoje, a Planner, sediada em Florianópolis, através de novas parcerias expandiu suas atividades e atende o mercado de  eletrônicos, dispomos  de  equipe de vendas atendendo  em todo o Estado de Santa Catarina e Rio Grande do Sul. <br/>
+							<p></p>
+						</div>
+						<div class="work_bottom"> <span>Quer saber mais..</span> <a href="#contact" class="contact_btn">Fale conosco</a> </div>
+					</div>
+
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+</section>
+<!--Aboutus-->
+
+
+<!--Service-->
+<section  id="service">
+	<div class="container">
+		<h2>Servicos</h2>
+		<div class="service_wrapper">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="service_block">
+						<div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa fa-ambulance"></i></span> </div>
+						<h3 class="animated fadeInUp wow">Laboratorios e Análises clínicas</h3>
+						<p class="animated fadeInDown wow">Representação junto a rede de laboratórios de Analises.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 borderLeft">
+					<div class="service_block">
+						<div class="service_icon icon2  delay-03s animated wow zoomIn"> <span><i class="fa fa-hospital-o"></i></span> </div>
+						<h3 class="animated fadeInUp wow">Farmácias e Hospitais</h3>
+						<p class="animated fadeInDown wow">Representação junto a Farmácias e hospitais e  Distribuidores.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 borderLeft">
+					<div class="service_block">
+						<div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-camera-retro"></i></span> </div>
+						<h3 class="animated fadeInUp wow">Produtos Eletrônicos</h3>
+						<p class="animated fadeInDown wow">Representação comercial de produtos eletrônicos.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 borderLeft">
+					<div class="service_block">
+						<div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-recycle"></i></span> </div>
+						<h3 class="animated fadeInUp wow">Produtos de Limpeza</h3>
+						<p class="animated fadeInDown wow">Distribuidor Independente de produtos de limpeza sustentável.</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+<!--Service-->
+
+<section class="page_section" id="clients"><!--page_section-->
+	<h2>Parcerias</h2>
+	<!--page_section-->
+	<div class="client_logos"><!--client_logos-->
+		<div class="container">
+			<ul class="fadeInRight animated wow">
+				<li><a href="http://www.dasa.com.br"><h1 style="color: white">DASA Diagnostico da América</h1></a></li>
+				<li><a href="http://www.oneletronicos.com.br"><h1 style="color: white" >On Eletrônicos</h1></a></li>
+				<li><a href="http://www.mundoirys.com.br"><h1 style="color: white" >Mundo Irys</h1></a></li>
+
+			</ul>
+		</div>
+	</div>
+</section>
+<!--client_logos-->
+
+
+<!--Footer-->
+<footer class="footer_wrapper" id="contact">
+	<div class="container">
+		<section class="page_section contact" id="contact">
+			<div class="contact_section">
+				<h2>Contato</h2>
+				<div class="row">
+					<div class="col-lg-4">
+
+					</div>
+					<div class="col-lg-4">
+
+					</div>
+					<div class="col-lg-4">
+
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 wow fadeInLeft">
+					<div class="contact_info">
+						<div class="detail">
+							<h4>Planner Representações</h4>
+							<p>RUA SOUZA DUTRA 145 SALA 612</p>
+							<p>BAIRRO ESTREITO</p>
+							<p>FLORIANOPOLIS SC</p>
+						</div>
+						<div class="detail">
+							<h4>FONE :</h4>
+							<p>48- 3091-2929</p>
+
+							<h4>CEL :</h4>
+							<p>CEL    :  48- 99944-4664</p>
+
+						</div>
+						<div class="detail">
+
+						</div>
+					</div>
+
+
+
+
+				</div>
+				<div class="col-lg-8 wow fadeInLeft delay-06s">
+
+                        
+						<!--NOTE: Update your email Id in "contact_me.php" file in order to receive emails from your contact form-->
+                        <?php if ($this->session->flashdata('success') == TRUE): ?>
+                            <div class="alert alert-success"><?= $this->session->flashdata('success'); ?></div>
+                        <?php endif; ?>
+
+                        <?php if ($this->session->flashdata('error') == TRUE): ?>
+                            <div class="alert alert-warning"><?= $this->session->flashdata('error'); ?></div>
+                        <?php endif; ?>
+                        <form method="POST" action="<?=base_url('enviar-email')?>">
+							<div class="control-group">
+								<div class="controls">
+									<input type="text" class="form-control input-text"
+										   placeholder="Nome completo" id="name" name="nome" required
+										   data-validation-required-message="Por favor entre com seu nome" />
+
+									<p class="help-block"></p>
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls">
+									<input type="email" class="form-control input-text" placeholder="Email"
+										   id="email" required
+										   data-validation-required-message="Por favor entre com um e-mail valido" />
+								</div>
+							</div>
+
+
+							<div class="control-group">
+								<div class="controls">
+						<textarea rows="10" cols="100" class="form-control input-text"
+								  placeholder="Mensagem" id="msg" required
+								  data-validation-required-message="Por favor entre com sua mensagem" minlength="5"
+								  data-validation-minlength-message="Min 5 characters"
+								  maxlength="999" style="resize:none">
+
+						</textarea>
+								</div>
+							</div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" name="anexo"/><strong>Enviar anexo</strong></label>
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" name="template"/><strong>Usar template</strong></label>
+                            </div>
+
+							<div id="success"> </div> <!-- For success/fail messages -->
+							<button type="submit" class="btn btn-primary input-btn pull-right" id="btn">Enviar</button><br />
+
+						</form>
+
+
+
+					</div>
+			</div>
+		</section>
+	</div>
+	<div class="container">
+		<div class="footer_bottom"><span>Copyright © 2016,    by <a href="">Digitec Sistemas</a>. </span> </div>
+	</div>
+</footer>
+
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.11.0.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-scrolltofixed.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.nav.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.easing.1.3.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.isotope.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/wow.js')?>"></script>
+<script src="<?php echo base_url('assets/contact/jqBootstrapValidation.js')?>"></script>
+<script src="<?php echo base_url('assets/contact/contact_me.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/custom.js')?>"></script>
+
+</body>
+</html>
